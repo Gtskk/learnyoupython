a94e0f8d38727c2be63ec3d9552799e4e46bd67f
Rock Zhang <iamgtskk@gmail.com>
2015-08-19 18:45:06 +0800

    更新部分learnyoupython代码

--- learnyounode.js
+++ learnyounode.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
 const workshopper = require('workshopper')
-    , path        = require('path')
-    , credits     = require('./credits')
+	, path        = require('path')
+	, credits     = require('./credits')
 
 function fpath (f) {
-  return path.join(__dirname, f)
+	return path.join(__dirname, f)
 }
 
 workshopper({
-      name        : 'learnyounode'
-    , appDir      : __dirname
-    , languages   : ['en', 'es', 'fr', 'ja', 'zh-cn', 'zh-tw', 'pt-br', 'ru', 'uk', 'vi', 'ko', 'nb-no']
-    , helpFile    : fpath('./i18n/help/{lang}.txt')
-    , menuItems   : [ {
-          name    : 'credits'
-        , handler : credits
-      } ]
-  })
+	name        : 'learnyoupython',
+	appDir      : __dirname,
+	languages   : ['zh-cn', 'en', 'es', 'fr', 'ja',  'zh-tw', 'pt-br', 'ru', 'uk', 'vi', 'ko', 'nb-no'],
+	helpFile    : fpath('./i18n/help/{lang}.txt'),
+	menuItems   : [{
+		name    : 'credits',
+		handler : credits
+	}]
+});
